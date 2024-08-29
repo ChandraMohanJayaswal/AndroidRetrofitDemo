87d3394e11367948b5bcf3d0f21a0549752d76b6
Aa-sh-sih <aashish33lamsal@gmail.com>
2024-08-29 12:40:08 +0545

    parsing of date and time

--- app/src/main/java/com/anu/todoretrofit/model/Todo.kt
+++ app/src/main/java/com/anu/todoretrofit/model/Todo.kt
@@ -8,35 +8,20 @@ import java.time.format.DateTimeFormatter
 
 data class Todo(
     val _id: String,
-    val created_at: ZonedDateTime ,
+    val created_at: String ,
     val is_completed: Boolean,
     val description: String,
     val title: String,
-    val updated_at: ZonedDateTime,
+    val updated_at: String,
 ){
 
-//    val createdAtZoned: ZonedDateTime
-//        @RequiresApi(Build.VERSION_CODES.O)
-//        get() = ZonedDateTime.parse(created_at, DateTimeFormatter.ISO_ZONED_DATE_TIME)
-//
-//    val updatedAtZoned: ZonedDateTime
-//        @RequiresApi(Build.VERSION_CODES.O)
-//        get() = ZonedDateTime.parse(updated_at, DateTimeFormatter.ISO_ZONED_DATE_TIME)
+    val createdAtZoned: ZonedDateTime
+        @RequiresApi(Build.VERSION_CODES.O)
+        get() = ZonedDateTime.parse(created_at, DateTimeFormatter.ISO_ZONED_DATE_TIME)
 
-    constructor(
-        _id: String,
-        created_at: String ,
-        is_completed: Boolean,
-        title: String,
-        description: String,
-        updated_at: String,
-    ):this(
-        _id = _id,
-        created_at = ZonedDateTime.parse(created_at, DateTimeFormatter.ISO_ZONED_DATE_TIME),
-        is_completed = is_completed,
-        title = title,
-        description=description,
-        updated_at = ZonedDateTime.parse(updated_at, DateTimeFormatter.ISO_ZONED_DATE_TIME)
-    )
+    val updatedAtZoned: ZonedDateTime
+        @RequiresApi(Build.VERSION_CODES.O)
+        get() = ZonedDateTime.parse(updated_at, DateTimeFormatter.ISO_ZONED_DATE_TIME)
+
 }
 
